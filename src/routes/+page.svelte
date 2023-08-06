d517147a96ca7f58b94a61db4e90390f6131ecff
Hedgy134117 <hedgy134117@gmail.com>
2023-08-06 15:41:17 -0400

    review component, basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,42 @@
 <script>
     // @ts-nocheck
     import reviews from "$lib/reviews.json";
+    import Review from './review.svelte';
     let names = Object.keys(reviews);
+    let places = ["The Joint Coffee Co", "Bitty & Beau's Coffee", "Billy's Downtown Diner", "Hotel B Ice Cream Parlor", "Chocolate Lab"]
 </script>
 
-{#each names as name}
-    <h1>{name}</h1>
-    {#each reviews[name] as review}
-        <li>{review}</li>
-    {/each}
+{#each places as place, i}
+    <div class="place">
+        <h1>{place}</h1>
+        <Review name="NAME" data={["PRODUCT", "PRICE", "FLAVOR", "VALUE", "AMBIANCE", "COMMENTS"]} />
+        {#each names as name}
+            <Review name={name} data={reviews[name][i]} />
+        {/each}
+    </div>
 {/each}
+
+<style>
+    :global(body) {
+        width: 60%;
+        margin: 0 auto;
+        padding: 1em;
+        background-color: #ffe5b4;
+        font-family: 'Imprima', sans-serif;
+    }
+
+    :global(p) {
+        margin: 0;
+    }
+
+    .place {
+        margin-bottom: 3em;
+    }
+
+    .place h1 {
+        font-size: 4em;
+        text-align: center;
+        text-transform: uppercase;
+        font-style: italic;
+    }
+</style>
---
+++ src/routes/review.svelte
@@ -0,0 +1,65 @@
+<script>
+    // @ts-nocheck
+    export let name;
+    export let data;
+
+    let product = data[0];
+    let price = data[1];
+    let flavor = data[2];
+    let value = data[3];
+    let ambiance = data[4];
+    let comments = data[5];
+
+    let lastRow = name == "Westley" ? true : false;
+</script>
+
+<div class:bottom={lastRow} class="outer">
+    <div class="cell">
+        <p class="">{name}</p>
+    </div>
+    <div class="cell">
+        <p class="">{product}</p>
+    </div>
+    <div class="cell">
+        <p class="">{price}</p>
+    </div>
+    <div class="cell">
+        <p class="">{flavor}</p>
+    </div>
+    <div class="cell">
+        <p class="">{value}</p>
+    </div>
+    <div class="cell">
+        <p class="">{ambiance}</p>
+    </div>
+    <div class="cell">
+        <p>{comments}</p>
+    </div>
+</div>
+
+<style>
+    .outer {
+        display: grid;
+        grid-template-columns: 10% 15% 7% 7% 7% 7% 47%;
+
+        border: 1px solid black;
+        border-bottom: 0;
+    }
+
+    .cell {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 1em;
+
+        border-right: 1px solid black;
+    }
+    
+    .cell:last-of-type {
+        border-right: 0;
+    }
+
+    .bottom {
+        border-bottom: 1px solid black;
+    }
+</style>
