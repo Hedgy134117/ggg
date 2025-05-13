663aca683b13c32fe4ab594262e03ea0a409f47d
Hedgy134117 <hedgy134117@gmail.com>
2025-05-13 12:19:05 -0400

    add sheets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,83 @@
 <script>
-    // @ts-nocheck
-    import reviews from "$lib/reviews.json";
-    import results from "$lib/results.json";
-    import Review from './review.svelte';
-    import Header from './header.svelte';
-	import Result from "./result.svelte";
-	import Predictions from "./predictions.svelte";
-    let names = Object.keys(reviews);
-    let places = ["The Joint Coffee Co", "Bitty & Beau's Coffee", "Billy's Downtown Diner", "Hotel B Ice Cream Parlor", "Chocolate Lab"]
+	// @ts-nocheck
+	import results from '$lib/results.json';
+	import reviews from '$lib/reviews.json';
+	import Header from './header.svelte';
+	import People from './people.svelte';
+	import Predictions from './predictions.svelte';
+	import Result from './result.svelte';
+	import Review from './review.svelte';
+	let names = Object.keys(reviews);
+	let places = [
+		'The Joint Coffee Co',
+		"Bitty & Beau's Coffee",
+		"Billy's Downtown Diner",
+		'Hotel B Ice Cream Parlor',
+		'Chocolate Lab'
+	];
 </script>
 
 <Header />
+<People />
 <Predictions />
 {#each places as place, i}
-    <div class="place">
-        <h1>{place}</h1>
-        <Review name="NAME" bolded=true data={["PRODUCT", "PRICE", "FLAVOR", "VALUE", "AMBIANCE", "COMMENTS"]} />
-        {#each names as name}
-            <Review name={name} data={reviews[name][i]} />
-        {/each}
-        <br />
-        <Result data={["AVG. PRICE", "AVG. FLAVOR", "AVG. VALUE", "AVG. AMBIANCE", "FAVORITE", "LEAST FAVORITE"]} />
-        <Result data={results[place]} />
-    </div>
+	<div class="place">
+		<h1>{place}</h1>
+		<Review
+			name="NAME"
+			bolded="true"
+			data={['PRODUCT', 'PRICE', 'FLAVOR', 'VALUE', 'AMBIANCE', 'COMMENTS']}
+		/>
+		{#each names as name}
+			<Review {name} data={reviews[name][i]} />
+		{/each}
+		<br />
+		<Result
+			data={[
+				'AVG. PRICE',
+				'AVG. FLAVOR',
+				'AVG. VALUE',
+				'AVG. AMBIANCE',
+				'FAVORITE',
+				'LEAST FAVORITE'
+			]}
+		/>
+		<Result data={results[place]} />
+	</div>
 {/each}
 
 <style>
-    :global(body) {
-        width: 80%;
-        margin: 0 auto;
-        background-color: #ffe5b4;
-        font-family: 'Imprima', sans-serif;
-    }
+	:global(body) {
+		width: 80%;
+		margin: 0 auto;
+		background-color: #ffe5b4;
+		font-family: 'Imprima', sans-serif;
+	}
 
-    :global(p) {
-        margin: 0;
-    }
+	:global(p) {
+		margin: 0;
+	}
 
-    :global(h1) {
-        font-size: 3em;
-        text-align: center;
-        text-transform: uppercase;
-        font-style: italic;
-    }
+	:global(h1) {
+		font-size: 3em;
+		text-align: center;
+		text-transform: uppercase;
+		font-style: italic;
+	}
 
-    .place {
-        margin-bottom: 3em;
-        overflow: auto;
-    }
+	.place {
+		margin-bottom: 3em;
+		overflow: auto;
+	}
 
-    .place h1 {    
-        position: sticky;
-        left: 0;
-    }
+	.place h1 {
+		position: sticky;
+		left: 0;
+	}
 
-    @media only screen and (max-width: 1300px) {
-        :global(body) {
-            width: 100%;
-        }
-    }
+	@media only screen and (max-width: 1300px) {
+		:global(body) {
+			width: 100%;
+		}
+	}
 </style>
---
+++ src/routes/people.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { base } from '$app/paths';
+	import reviews from '$lib/reviews.json';
+	const people = Object.keys(reviews);
+	const missing = ['Ivy', 'Arianna', 'Sam'];
+</script>
+
+<h1>PEOPLE</h1>
+<div>
+	{#each people as person}
+		{#if missing.includes(person)}
+			<p>{person}</p>
+		{:else}
+			<p><a href={base + '/' + person.toLowerCase() + '.pdf'} target="_blank">{person}</a></p>
+		{/if}
+	{/each}
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	p {
+		font-size: 1.5em;
+	}
+</style>
